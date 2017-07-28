1e56e3c833e3bb933c6f8c902403cb07cfd6146a
Vinicius Carlos Oliveira de Andrade <vinivice@gmail.com>
2017-07-28 11:22:46 -0300

    Update main.c
    Added identifiers enum

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
 #include<string.h>
 
+typedef enum {
+// palavras reservadas
+ARRAY, BOOLEAN, BREAK, CHAR, CONTINUE, DO, ELSE, FALSE, FUNCTION, IF,
+INTEGER, OF, STRING, STRUCT, TRUE, TYPE, VAR, WHILE,
+// simbolos
+COLON, SEMI_COLON, COMMA, EQUALS, LEFT_SQUARE, RIGHT_SQUARE,
+LEFT_BRACES, RIGHT_BRACES, LEFT_PARENTHESIS, RIGHT_PARENTHESIS, AND,
+OR, LESS_THAN, GREATER_THAN, LESS_OR_EQUAL, GREATER_OR_EQUAL,
+NOT_EQUAL, EQUAL_EQUAL, PLUS, PLUS_PLUS, MINUS, MINUS_MINUS, TIMES,
+DIVIDE, DOT, NOT,
+// tokens regulares
+CHARACTER, NUMERAL, STRINGVAL, ID,
+// token deconhecido
+UNKNOWN } t_token;
+
 void printUsage(char *arg)
 {
         printf("Invalid mode!\n");
